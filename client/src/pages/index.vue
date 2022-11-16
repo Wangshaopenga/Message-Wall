12bde5cda5aa43fc41257f8cd5a6a603489276fb
Wangshaopenga <483041241@qq.com>
2022-11-16 16:49:52 +0800

    first commit

---
+++ client/src/pages/index.vue
@@ -0,0 +1,13 @@
+<!-- eslint-disable vue/singleline-html-element-content-newline -->
+<template>
+  <div h-full bg="#edeff1" text-center flex="~" justify-center items-center>
+    <div>
+      <div>{{ store.num }}</div>
+      <button red-btn @click="store.num++">+</button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const store = useStore()
+</script>
