b055082eae549396b02c5e9fe8c43e3b60f0efaf
Wangshaopenga <483041241@qq.com>
2022-11-19 15:12:23 +0800

    完成留言墙部分

--- client/src/components/TopBar.vue
+++ client/src/components/TopBar.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="bg-#f5f5f7 shadow h-52px w100% flex items-center justify-between relative dark:bg-#222 dark:shadow">
-    <div class="relative inset-l-1.5% flex-[1] flex h100%  items-center text-4 text-center leading-52px font-700 select-none overflow-hidden">
-      <img w32px h32px cursor-pointer hover:scale-110 transition-transform-500 src="@/assets/images/logo.svg" alt="">
-      <span class="cursor-pointer  pl2 whitespace-nowrap display-none text-20px md:block">一刻时光</span>
+  <div class="topbar">
+    <div class="logo">
+      <img class="logo-img" src="@/assets/images/logo.svg" alt="">
+      <span class="logo-title">一刻时光</span>
     </div>
 
-    <div class="flex relative justify-center h100% items-center">
-      <button class="w82px h36px m1 text-center leading-36px cursor-pointer select-none" :class="{ btnActive: isActive === 1 }" @click="goMessage">留言墙</button>
-      <button class="w82px h36px m1 text-center leading-36px cursor-pointer select-none" :class="{ btnActive: isActive === 2 }" @click="goImages">照片墙</button>
+    <div class="btns">
+      <button class="top-btn" :class="{ btnActive: isActive === 1 }" @click="goMessage">留言墙</button>
+      <button class="top-btn" :class="{ btnActive: isActive === 2 }" @click="goImages">照片墙</button>
     </div>
 
-    <div class="flex h100% relative inset-r-1.5% flex-[1] overflow-hidden items-center justify-end">
-      <img class="cursor-pointer  hover:scale-110 transition-transform-500 w35px h35px mr1.35rem rd-50%" src="@/assets/images/1.jpg" alt="">
-      <span v-show="isDark" class="i-carbon-moon hover:scale-110 transition-transform-500 text-5 relative cursor-pointer" @click="toggleDark()" />
-      <span v-show="!isDark" class="i-carbon-sun hover:scale-110 transition-transform-500 text-5 relative cursor-pointer" @click="toggleDark()" />
+    <div class="user">
+      <img class="avatar" src="@/assets/images/1.jpg" alt="">
+      <span v-show="isDark" class="i-carbon-moon toggle" @click="toggleDark()" />
+      <span v-show="!isDark" class="i-carbon-sun toggle" @click="toggleDark()" />
     </div>
   </div>
 </template>
@@ -34,6 +34,33 @@ function goImages() {
 </script>
 
 <style scoped lang="scss">
+.topbar {
+  @apply bg-#f5f5f7 shadow h-52px w100% flex items-center justify-between relative dark:bg-#222 dark:shadow  fixed z1;
+  .logo {
+    @apply relative inset-l-1.5% flex-[1] flex h100%  items-center text-4 text-center leading-52px font-700 select-none overflow-hidden;
+    .logo-img {
+      @apply w32px h32px cursor-pointer hover:scale-110 transition-transform-500;
+    }
+    .logo-title {
+      @apply cursor-pointer  pl2 whitespace-nowrap display-none text-20px md:block;
+    }
+  }
+  .btns {
+    @apply flex relative justify-center h100% items-center;
+    .top-btn {
+      @apply w82px h36px m1 text-center leading-36px cursor-pointer select-none;
+    }
+  }
+  .user {
+    @apply flex h100% relative inset-r-1.5% flex-[1] overflow-hidden items-center justify-end;
+    .avatar {
+      @apply cursor-pointer  hover:scale-110 transition-transform-500 w35px h35px mr1.35rem rd-50%;
+    }
+    .toggle {
+      @apply hover:scale-110 transition-transform-500 text-5 relative cursor-pointer;
+    }
+  }
+}
 .btnActive {
   @apply bg-[#3B73F0] rd-5 text-white text-[4.5] transition-bg transition-colors transition-500 hover:bg-[#5069f0]
   background: rgba(80, 105, 240);
--- client/src/views/wall/message.vue
+++ client/src/views/wall/message.vue
@@ -1,28 +1,93 @@
 <template>
-  <div class="h100p w100p flex p56px flex-col bg-#f2f2f2 items-center dark:bg-#1e1e1e">
-    <div text-center>
-      <p text-56px font-600>留言墙</p>
-      <p class="text-14px color-#5b5b5b font-400">很多事情值得记录，当然也值得回味。</p>
+  <div class="message-wall">
+    <div class="top">
+      <p class="title">留言墙</p>
+      <p class="quote">很多事情值得记录，当然也值得回味。</p>
     </div>
-    <div py10 flex flex-wrap>
-      <div class="category" @click="isActive = index" :class="{ active: index === isActive }" v-for="(item, index) in categories" :key="item">{{ item }}</div>
+    <div class="category">
+      <div class="item" @click="isActive = index" :class="{ active: index === isActive }" v-for="(item, index) in categories" :key="item">{{ item }}</div>
     </div>
-    <div class="self-start ma grid md:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
-      <div class="w288px h240px bg-#d9edd1  py12px px20px m6px" v-for="i in 10" :key="i">{{ i }}</div>
+    <div class="main">
+      <div class="card-box" v-for="i in 10" :key="i">
+        <div class="card-top">
+          <p class="">22-11-18 21:09</p>
+          <p class="">爱情</p>
+        </div>
+        <div class="card-content">{{ content }}</div>
+        <div class="card-bottom">
+          <div class="icones">
+            <div class="icon">
+              <div class="i-carbon-favorite-filled heart" />
+              <div ml1>1</div>
+            </div>
+            <div class="icon">
+              <div class="i-carbon-chat-bot comment" />
+              <div ml1>1</div>
+            </div>
+          </div>
+          <div class="name">匿名</div>
+        </div>
+      </div>
     </div>
+    <div class="loading">没有更多了~</div>
   </div>
 </template>
 
 <script setup lang='ts'>
 const categories: string[] = ['全部', '留言', '目标', '理想', '过去', '将来', '爱情', '亲情', '友情', '秘密', '信条', '无题']
 let isActive = $ref(0)
+let color = ref('#e1f6d8')
+const content = $ref('这是一段暖心的话，它或许不长，但是它是我现在最想说的。放在这里就留一个纪念吧，不用回头看，应为现在才是当下最好的。这是一段暖心的话，它或许不长，但是它是我现在最想说的。放在这里就留一个纪念吧。')
 </script>
 
 <style lang="scss" scoped>
-.category {
-  @apply color-#5b5b5b h28px w56px leading-28px font-500 text-14px m1 cursor-pointer text-center transition-font-300;
-}
-.active {
-  @apply color-#202020 border-#202020 border-1  font-600 rd-5 dark:color-#fefefe dark:border-gray-300;
+.message-wall {
+  @apply w100% flex p56px flex-col bg-#f2f2f2 items-center justify-center dark:bg-#1e1e1e;
+  .top {
+    @apply text-center;
+    .title {
+      @apply text-56px font-600;
+    }
+    .quote {
+      @apply text-14px color-#5b5b5b font-400;
+    }
+  }
+  .category {
+    @apply py10 flex flex-wrap;
+    .item {
+      @apply color-#5b5b5b h28px w56px leading-28px font-500 text-14px m1 cursor-pointer text-center transition-font-300;
+    }
+    .active {
+      @apply color-#202020 border-#202020 border-1  font-600 rd-5 dark:color-#fefefe dark:border-gray-300;
+    }
+  }
+  .main {
+    @apply self-start ma grid gap2 md:grid-cols-2 xl:grid-cols-4 relative lg:grid-cols-3;
+    .card-box {
+      @apply w288px h240px py12px px24px rd-5px shadow relative m6px hover:translate-y--1.5 transition duration-300;
+      background-color: v-bind(color);
+      .card-top {
+        @apply color-#949494 text-12px top-5% flex justify-between;
+      }
+      .card-content {
+        @apply text-14px absolute w246px h140px text-#202020 indent-2em top-20% left-50% translate-x--50%;
+      }
+      .card-bottom {
+        @apply flex justify-between w-240px p1 absolute bottom-2%;
+        .icones{
+          @apply flex relative text-14px text-#949494 text-center;
+          .icon {
+            @apply flex mr2 cursor-pointer first:hover:text-red;
+          }
+        }
+        .name {
+          @apply text-14px italic text-#202020;
+        }
+      }
+    }
+  }
+  .loading {
+    @apply mt10 w100p text-center;
+  }
 }
 </style>
